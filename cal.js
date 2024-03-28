3f4a2dd42d3bf75c205e96f4e05d5d5ac5683cbb
eimlfang <eimlfang@126.com>
2024-03-28 18:18:30 +0800

    20240328

---
+++ cal.js
@@ -0,0 +1,37 @@
+let ycyth = [
+        6,
+        3,
+        4,
+        2,
+        4,
+        2,
+        
+]
+
+let sum = 0;
+for(let i = 0; i < ycyth.length; i++) {
+sum += ycyth[i];
+}
+console.log('业财一体化 = ' + sum)
+
+let bg = [
+1,
+8,
+2,
+3,
+2,
+1,
+2,
+2,
+1,
+1,
+1,
+1,
+2
+]
+
+let sum2 = 0;
+for(let i = 0; i < bg.length; i++) {
+sum2 += bg[i];
+}
+console.log('变更总天数 = ' + sum2)
--- test.md
+++ test.md
@@ -1,1 +1,15 @@
-FangZijian
+1. "pbzmb1.mp4",
+2. "pbzmb2.mp4",
+3. "pbzmb3.mp4",
+4. "pbzmb4.mp4",
+5. "pbzmb5.mp4",
+6. "pbzmb6.mp4",
+7. "pbzmb7.mp4",
+8. "pbzmb8.mp4",
+9. "pbzmb9.mp4",
+10. "pbzmb10.mp4"
+
+
+{"bcUtE": 43, "shMxD": 70, "NexYc": 76, "pbzmb": 69, "ADNke": 44, "ZkKVg": 26, "phbIO": 46, "CKzQf": 80, "JujfE": 83, "XlQGT": 91}
+
+
